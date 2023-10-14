7278c4e9ec8902e9f6f326594ed360297f1b3276
Gevorg Minasian <cool.port-965@yandex.ru>
2023-10-14 23:39:12 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import math
+# start = int(input("Введите начало диапазона: "))
+# end = int(input("Введите конец диапазона: "))
+#
+# print("Простые числа в диапазоне от", start, "до", end, "включительно:")
+#
+# for num in range(start, end + 1):
+#     if num > 1:
+#         is_prime = True
+#         for i in range(2, int(num ** 0.5) + 1):
+#             if num % i == 0:
+#                 is_prime = False
+#                 break
+#         if is_prime:
+#             print(num)
+
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         result = i * j
+#         print(f"{i} * {j} = {result}")
+#     print("...............................")
+
+
+# start = int(input("Введите начало диапазона: "))
+# end = int(input("Введите конец диапазона: "))
+#
+# for i in range(start, end + 1):
+#     for j in range(1, 11):
+#         result = i * j
+#         print(f"{i} * {j} = {result}")
+#     print("...............................")
